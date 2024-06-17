0beb58f34afb155efe974968f6868e0103431dc4
Dumble@1099 <sunilkumartalluri1234@gmail.com>
2024-06-17 08:25:49 +0530

    some changes

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,21 +1,22 @@
 public class RewardValue {
      double cash;
      double miles;
-    RewardValue(int miles){
+
+    public RewardValue(double cash) {
+        this.cash = cash;
+        this.miles = cash / 0.0035;
+    }
+     RewardValue(int miles){
         this.miles = miles;
         this.cash = miles * 0.0035;
     }
 
-    public RewardValue(double cashValue) {
-        this.cash = cash;
-        this.miles = cash / 0.0035;
-    }
-
     public double getCashValue() {
+
         return cash;
     }
 
     public double getMilesValue() {
-        return miles;
+        return miles ;
     }
 }
